b79cbd285600a3194121095dd46d6d649594540d
IamLAM <ingeniero@luismonge.com.mx>
2018-05-14 17:35:56 -0700

    Desplegar por boxplot,scatterplot e histograma
    Se cargan datos y se despliegan en graficas

--- graficar_valores.R
+++ graficar_valores.R
@@ -1,1 +1,13 @@
 library(datasets)
+
+# desplegando valores por histograma
+hist(airquality$Ozone)
+
+#desplegando valores por scatterplot
+with(airquality, plot(Wind, Ozone))
+
+
+#desplegando valores por boxplot
+airquality <- transform(airquality, Month = factor(Month)) 
+boxplot(Ozone ~ Month, airquality, xlab = "Mes", ylab = "Ozono (ppb)")
+
